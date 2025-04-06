819c407b5fe64cf2aca534366f6eca4f363a420a
MiguelAngelGRomero <miguel.gomez1704@alumnos.udg.mx>
2025-04-06 10:11:23 -0600

    Se agregaron portadas

---
+++ scriptPeliculas.js
@@ -0,0 +1,33 @@
+function cargarPeliculas() {
+    fetch("peliculas.json") //fetch para sacar archivos del json
+        .then(respuesta => respuesta.json()) //almacenamos la respuesta
+        .then(function(salida) { //funcion anonima para crear el div
+            let contenedor = document.getElementById("peliculas"); //se obtiene el id del div que esta en el documento
+            for (let item of salida.contenido) {
+                let div = document.createElement("div"); //se crea el elemento div con las imagenes y botones
+                div.className = "divContenido";
+                div.innerHTML = `
+                    <img class="imagen" src="${item.imagen}" alt="cargando...">
+                    <p>${item.titulo}</p>
+                    <button onclick="meGusta('${item.titulo}')">Me gusta</button>
+                    <button onClick="ocultar(this)">Ocultar</button>
+                    <button>Favorito</button>
+                `;
+                contenedor.appendChild(div);  //se agrega al contenedor como hijos
+            }
+        })
+        .catch(function(error) {  //se captura el error si llegara a pasar
+            console.error("Error al cargar JSON:", error);
+        });
+}
+
+cargarPeliculas();
+
+function meGusta(titulo){   //Te muestra en una alerta que te gusto el nombre de la serie
+    alert(`¡Te gustó ${titulo}!`);
+}
+
+function ocultar(boton){  //oculta
+    let contenedor = boton.parentElement;
+    contenedor.style.display="None";
+}
